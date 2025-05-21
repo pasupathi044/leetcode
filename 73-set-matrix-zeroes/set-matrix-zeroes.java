996699517865a11b7816d588abd1f8cf8adface1
pasupathi044 <124163925+pasupathi044@users.noreply.github.com>
2025-05-21 21:52:02 +0530

    Time: 1 ms (72.83%) | Memory: 46 MB (8.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -6,15 +6,15 @@ class Solution {
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    row[i]=-1;
+                    col[j]=-1;
                 }
             }
         }
         //marking zeroes
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(row[i]==1 || col[j]==1){
+                if(row[i]==-1 || col[j]==-1){
                     matrix[i][j]=0;
                 }
             }
