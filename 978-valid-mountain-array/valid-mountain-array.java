7b7fed0b853779060d3ae46cfb5bbf6680e8328d
pasupathi044 <124163925+pasupathi044@users.noreply.github.com>
2025-07-21 14:25:56 +0530

    Time: 2 ms (69.99%) | Memory: 45.2 MB (87.09%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.java
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -3,17 +3,9 @@ class Solution {
         int n = arr.length;
         if (n < 3) return false;
         int i = 0;
-        
-        // Walk up
         while (i + 1 < n && arr[i] < arr[i + 1]) i++;
-        
-        // Peak can't be first or last
         if (i == 0 || i == n - 1) return false;
-        
-        // Walk down
         while (i + 1 < n && arr[i] > arr[i + 1]) i++;
-        
-        // Did we reach the end?
         return i == n - 1;
     }
 }
