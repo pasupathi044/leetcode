8c02900e89a40d9befcfe986427abf4e038c500d
pasupathi044 <124163925+pasupathi044@users.noreply.github.com>
2025-08-18 14:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (49.87%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,24 @@
-class Solution {
-    public int trap(int[] height) {
-        int[]leftMax = new int[height.length]; 
-        int[]rightMax = new int [height.length];
-        int total=0;
-        // left max array 
-        leftMax[0]=height[0];
-        for(int i=1;i< height.length;i++){
-            leftMax[i]=Math.max(leftMax[i-1],height[i]);
-        } 
-
-        // right max array 
-        rightMax[height.length-1]=height[height.length-1];
-        for(int i=height.length-2;i>=0;i--){
-            rightMax[i]=Math.max(rightMax[i+1],height[i]);
-        } 
-
-        //find the total puddle
-        for(int i=0;i<height.length;i++){
-            if(height[i]<leftMax[i] && height[i]<rightMax[i]){
-                total+=Math.min(leftMax[i],rightMax[i])-height[i];
+class Solution{
+public int trap(int[] height) {
+    int left = 0, right = height.length - 1;
+    int leftMax = 0, rightMax = 0, total = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                total += leftMax - height[left];
             }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                total += rightMax - height[right];
+            }
+            right--;
         }
-
-        return total;
     }
+    return total;
+}
 }
